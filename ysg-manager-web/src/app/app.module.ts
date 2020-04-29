40978dcd617dba6ed8d55943f5a69258c5de085d
Lukas Eberli <l.eberli@sunrise.ch>
2020-04-29 22:09:47 +0200

    add core module

--- ysg-manager-web/src/app/app.module.ts
+++ ysg-manager-web/src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppComponent} from './app.component';
+import {CoreModule} from "./core/core.module";
 
+/**
+ * The AppModule which will stay virtually empty for the whole life-time of the project,
+ * everything that needs to be available from start will be added to the CoreModule.
+ */
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ ysg-manager-web/src/app/core/core.module.ts
@@ -0,0 +1,17 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from "@angular/platform-browser";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+
+/**
+ * Core module with basic layout and all the core singleton services which will
+ * be used throughout the whole application.
+ */
+@NgModule({
+  declarations: [],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule
+  ]
+})
+export class CoreModule {
+}
