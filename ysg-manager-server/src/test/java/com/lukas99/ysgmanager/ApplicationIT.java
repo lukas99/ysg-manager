39acbb7ca7f70276b184ac93741981a2a3c9fde7
Lukas Eberli <l.eberli@sunrise.ch>
2020-04-29 20:34:07 +0200

    execute unit and integration tests during build
    and ensure all integration tests end with 'IT'

--- ysg-manager-server/src/test/java/com/lukas99/ysgmanager/ApplicationIT.java
+++ ysg-manager-server/src/test/java/com/lukas99/ysgmanager/ApplicationIT.java
@@ -3,7 +3,7 @@ package com.lukas99.ysgmanager;
 import com.lukas99.ysgmanager.adapter.rest.IntegrationTest;
 import org.junit.jupiter.api.Test;
 
-public class ApplicationTests extends IntegrationTest {
+public class ApplicationIT extends IntegrationTest {
 
   @Test
   public void contextLoads() {
