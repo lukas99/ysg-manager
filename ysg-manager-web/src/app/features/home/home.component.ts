26d57baf88d99fe07d9d465b56c8f73a8cb6a4f1
Lukas Eberli <l.eberli@sunrise.ch>
2022-10-14 21:21:57 +0200

    change front page text

--- ysg-manager-web/src/app/features/home/home.component.ts
+++ ysg-manager-web/src/app/features/home/home.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ysg-home',
-  template: `<p>home works!</p>`,
+  template: `<h1 style="padding: 30px">Welcome to the YSG Manager!</h1>`,
   styleUrls: []
 })
 export class HomeComponent {
