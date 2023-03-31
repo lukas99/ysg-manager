93e7df29056b33360bc762dc3180a33eff137990
Lukas Eberli <l.eberli@sunrise.ch>
2023-03-31 09:17:35 +0200

    remove no more needed dependency 'uuid'

--- ysg-manager-web/setup-jest.ts
+++ ysg-manager-web/setup-jest.ts
@@ -1,9 +1,1 @@
 import 'jest-preset-angular/setup-jest';
-import crypto from 'crypto'
-
-// otherwise creating a UUID throws an error during tests
-Object.defineProperty(global, 'crypto', {
-  value: {
-    getRandomValues: (arr:any) => crypto.randomBytes(arr.length)
-  }
-});
